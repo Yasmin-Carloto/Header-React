17a693d4126679337afa86b18562044fc2bd0ebe
Yasmin-Carloto <yasmincarlotobezerra17@gmail.com>
2024-08-03 16:31:12 -0300

    feat: complete header

---
+++ src/components/Content/Content.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+import "./Content.css"
+
+const Content = ({contentText}) => {
+    return (
+        <p className="content">{contentText}</p>
+    )
+}
+
+export default Content
